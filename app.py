7e64953710cc65cabee1ac3405e54370d0920497
lordraven-001 <79086545+lordraven-001@users.noreply.github.com>
2021-02-24 14:57:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome! This is a customization!! Version 2"
+    return "Welcome! This is a customization!! Version 3"
 
 @app.route('/how are you')
 def hello():
