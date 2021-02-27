8e5c97c2e4156365bb319e567e2278b90fd1e1ff
lordraven-001 <79086545+lordraven-001@users.noreply.github.com>
2021-02-27 15:48:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome! This is a customization!! Version 9"
+    return "Isten hozott! Ez egy módosítás!! Version 9"
 
-@app.route('/how are you')
+@app.route('/hogy vagy')
 def hello():
-    return 'I am good, how about you?'
+    return 'Köszönöm jól, s hogy vagy Te?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
